a66a6966b3027b409f2ce088c34bfa46eae6fb9b
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-10-21 16:40:16 +0530

    Added: Gambler is Win or Loose

--- src/com/program/GamblingGame.java
+++ src/com/program/GamblingGame.java
@@ -5,15 +5,22 @@ public class GamblingGame {
 	public void gamble() {
 		System.out.println("Start The Game");
 		int gambleAmt=100;
-		double bet=Math.random();
-		if (bet >= 0.50) {           //  for above 50% it will get the bet is Win
+		int winingAmt=0;
+		int looseAmt=0;
+		
+		while(winingAmt <= 50 && looseAmt >= -50)  {
+			double bet=Math.random();
+		if (bet >= 0.50) {
 			gambleAmt=gambleAmt+1;
+			winingAmt=winingAmt+1;
 			System.out.println("The Bet is Win & Total Amount is  "+gambleAmt);
 		}
-		else  {                    //  for below 50% it will get the bet is loose
+		else  {
 			gambleAmt=gambleAmt-1;
+			looseAmt=looseAmt-1;
 			System.out.println("The Bet is loose & Total Amount is  "+gambleAmt);	
-		}		
+		}
+		}
 		}
 	public static void main(String [] args) {
 		GamblingGame bet_Obj=new GamblingGame();
