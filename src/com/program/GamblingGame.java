e775e1abbd78aba558f7a97052b9cd0f1d30e23e
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-10-21 16:53:32 +0530

    Refactor: Gambler is Playe 2 Month  & Luckies/Unluckiet Day Data Collect

--- src/com/program/GamblingGame.java
+++ src/com/program/GamblingGame.java
@@ -4,22 +4,23 @@ import java.util.Arrays;
 
 public class GamblingGame {
 	
-	public void gamble() {
+	public void gamble(int num) {
 		System.out.println("Start The Game");
-		 int array[]=new int[20];   //  Store the value of the current month
+		while (num >= 1)  {   //  This Loop is For continue to next month or not
+	        int array[]=new int[20];  //  Store the value of the current month
 	        int i=0;
-			int days=20;           
-			while(days >= 1) {        //    this loop is to try 20 days gambling
+			int days=20;
+			while(days >= 1) {      //    this loop is to try 20 days gambling
 				days--; 
 				int gambleAmt=100;
 				int winingAmt=0;  int looseAmt=0;
-				while(winingAmt <= 50 && looseAmt >= -50)  {
+				while(winingAmt <= 50 && looseAmt >= -50)  {   //  for 50% amount will loose or win
 					double bet=Math.random();
-					if (bet >= 0.50) {            //  for above 50% it will get the bet is Win
+					if (bet >= 0.50) {                  //  for above 50% it will get the bet is Win
 						gambleAmt=gambleAmt+1;
 						winingAmt=winingAmt+1;
 					}
-					else  {                 //  for below 50% it will get the bet is loose
+					else  {                     //  for below 50% it will get the bet is loose
 						gambleAmt=gambleAmt-1;
 						looseAmt=looseAmt-1;
 					}
@@ -32,9 +33,13 @@ public class GamblingGame {
 			Arrays. sort(array);
 			System.out.println("The Highest Bet Loose in The Month  & Unlucku day loose Amount is  " +array[0]);
 					System.out.println("The Highest Bet Win in The Month & lucku day profit Amount is   is  "+array[19]);
-		}
+					num--;
+			}
+			}
 	public static void main(String [] args) {
 		GamblingGame bet_Obj=new GamblingGame();
-		bet_Obj.gamble();		
+	//  pass the  for argument to continue how many months in person
+		//  put the value in month how many month it will played the BET
+		bet_Obj.gamble(2);		
 	}
 }
