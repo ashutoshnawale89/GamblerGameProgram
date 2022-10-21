da61958db02728c17ef3b49eb83b3816c2a7e906
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-10-21 16:30:27 +0530

    Added: Welcome msg

---
+++ src/com/program/GamblingGame.java
@@ -0,0 +1,13 @@
+package com.program;
+
+public class GamblingGame {
+	public void gamble() {
+		
+			System.out.println("Welcome To Gamble Game");	
+		}		
+		
+	public static void main(String [] args) {
+		GamblingGame bet_Obj=new GamblingGame();
+		bet_Obj.gamble();		
+	}
+}
