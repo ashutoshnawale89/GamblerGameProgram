c1956fd4915051ebf403d1920e9db6711c8e441b
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-10-21 16:33:29 +0530

    Added: Gambler start The Bet

--- src/com/program/GamblingGame.java
+++ src/com/program/GamblingGame.java
@@ -2,10 +2,17 @@ package com.program;
 
 public class GamblingGame {
 	public void gamble() {
-		
-			System.out.println("Welcome To Gamble Game");	
+		int gambleAmt=100;
+		double bet=Math.random();
+		if (bet >= 0.50) {         //  for above 50% it will get the bet is Win
+			gambleAmt=gambleAmt+1;
+			System.out.println("The Bet is Win & Total Amount is  "+gambleAmt);
+		}
+		else  {          //  for below 50% it will get the bet is loose
+			gambleAmt=gambleAmt-1;
+			System.out.println("The Bet is loose & Total Amount is  "+gambleAmt);	
 		}		
-		
+		}
 	public static void main(String [] args) {
 		GamblingGame bet_Obj=new GamblingGame();
 		bet_Obj.gamble();		
