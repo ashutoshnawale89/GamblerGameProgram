9130aa52e771b23a8ea9cef1940c9806301ed403
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-10-21 16:45:12 +0530

    Refactor: Gambler is Playe 20 Days & Each Day Data Collect

--- src/com/program/GamblingGame.java
+++ src/com/program/GamblingGame.java
@@ -4,16 +4,14 @@ public class GamblingGame {
 	
 	public void gamble() {
 		System.out.println("Start The Game");
-		int gambleAmt=100;
-		int winingAmt=0;
-		int looseAmt=0;
 		int days=20;
-		while(days >= 1) {    //    this loop is to try 20 days gambling
+		while(days >= 1) {   //    this loop is to try 20 days gambling
 		days--; 
-		winingAmt=0;  looseAmt=0;
+		int gambleAmt=100;
+		int winingAmt=0;  int looseAmt=0;
 		while(winingAmt <= 50 && looseAmt >= -50)  {
 			double bet=Math.random();
-		if (bet >= 0.50) {            //  for above 50% it will get the bet is Win
+		if (bet >= 0.50) {          //  for above 50% it will get the bet is Win
 			gambleAmt=gambleAmt+1;
 			winingAmt=winingAmt+1;
 		}
@@ -22,6 +20,7 @@ public class GamblingGame {
 			looseAmt=looseAmt-1;
 		}
 		}
+		System.out.println("The Bet is loose & win in the Days is "+looseAmt+" & "+winingAmt+" Respectively");
 		System.out.println("The Bet of day the & Total Amount is  "+gambleAmt);
 		}
 	}
