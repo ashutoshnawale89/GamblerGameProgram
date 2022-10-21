d600f05010aa029c4249044d76ae8bb0718875b6
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-10-21 16:36:54 +0530

    Added: Gambler start The 1 Doller Bet

--- src/com/program/GamblingGame.java
+++ src/com/program/GamblingGame.java
@@ -1,14 +1,16 @@
 package com.program;
 
 public class GamblingGame {
+	
 	public void gamble() {
+		System.out.println("Start The Game");
 		int gambleAmt=100;
 		double bet=Math.random();
-		if (bet >= 0.50) {         //  for above 50% it will get the bet is Win
+		if (bet >= 0.50) {           //  for above 50% it will get the bet is Win
 			gambleAmt=gambleAmt+1;
 			System.out.println("The Bet is Win & Total Amount is  "+gambleAmt);
 		}
-		else  {          //  for below 50% it will get the bet is loose
+		else  {                    //  for below 50% it will get the bet is loose
 			gambleAmt=gambleAmt-1;
 			System.out.println("The Bet is loose & Total Amount is  "+gambleAmt);	
 		}		
